b5006581c753ca848af2c230afa2b61ad0c11ddc
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-03-04 00:01:05 -0600

    Added submit functions to controllers. Added logic to form validation

--- assets/js/app/controller.js
+++ assets/js/app/controller.js
@@ -1,7 +1,43 @@
 angular.module('app.controllers', [])
 .controller('LoginCtrl', function($scope) {
 
+
+// $scope.emailCheck = function(identifier) {
+// 	console.log(identifier);
+// };
+
+// $scope.passwordCheck = function(password) {
+// 	console.log(password);
+// };
+
+$scope.submit = function(identifier, password) {
+
+	var loginUser = {
+		identifier: identifier,
+		password: password
+	};
+	console.log(loginUser);
+};
+
 })
 .controller('RegisterCtrl', function($scope) {
+	// $scope.username = "";
+	// $scope.identifier = "";
+	// $scope.password = "";
+
+
+
+
+
+	$scope.submit = function(username, identifier, password) {
+
+		var registerUser = {
+			username: username,
+			identifier: identifier,
+			password: password
+		};
+		console.log(registerUser);
+	};
+
 
 });
